f5b25b41299c1cca0bd9f9d5e9eaec07f2cf2025
its-kaushik <mkaushik0033@gmail.com>
2022-03-26 13:37:52 +0530

    added the MarketStats router

--- routes/coinRankingRoutes.js
+++ routes/coinRankingRoutes.js
@@ -3,6 +3,7 @@ const coinRankingController = require('../controllers/coinRankingController') ;
 
 const router = Router() ;
 
+router.get('/marketstats', coinRankingController.getMarketStats) ;
 router.get('/coins/:numOfCoins', coinRankingController.getCoins) ;
 
 module.exports = router ;
