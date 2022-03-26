d5f84feb7822a88e610081201a7fbc6dde15c121
its-kaushik <mkaushik0033@gmail.com>
2022-03-26 13:34:34 +0530

    renamed routes in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express') ;
 const cors = require('cors') ;
 
-const coinsRoutes = require('./routes/coins') ;
+const coinRankingRoutes = require('./routes/coinRankingRoutes') ;
 const app = express() ;
 app.use(cors()) ;
 app.use(express.json()) ;
 
-app.use('/api', coinsRoutes) ;
+app.use('/api', coinRankingRoutes) ;
 
 app.listen(4000, () => {
     console.log("Listening to port 4000") ;
