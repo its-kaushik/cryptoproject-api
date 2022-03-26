d506fe92abc60491d679e8379d7c2d976aa365da
its-kaushik <mkaushik0033@gmail.com>
2022-03-26 13:35:24 +0530

    deleted files

--- routes/coins.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express') ;
-const coinsController = require('../controllers/coinsController') ;
-
-const router = Router() ;
-
-router.get('/coins/:numOfCoins', coinsController.getCoins) ;
-
-module.exports = router ;
